2c1607fd2323ec070521790c8c93780e58ed9421
Ariana Lopez <arilopezcode@gmail.com>
2026-08-12 18:28:45 +0000

    added ml.predict code for k=4

--- ml_clustering/bqml_clustering_code.sql
+++ ml_clustering/bqml_clustering_code.sql
@@ -96,7 +96,7 @@ SELECT 'k=5', davies_bouldin_index FROM ML.EVALUATE(MODEL `bq-sandbox-ariana.nyc
 UNION ALL
 SELECT 'k=6', davies_bouldin_index FROM ML.EVALUATE(MODEL `bq-sandbox-ariana.nyc_taxi_ml.zone_clusters_k6`);
 
---checking cluster info, k=6
+--checking cluster info, k=4
 SELECT
   feature,
   ROUND(MAX(IF(centroid_id = 1, numerical_value, NULL)), 1) AS cluster_1,
@@ -107,7 +107,7 @@ FROM ML.CENTROIDS(MODEL `bq-sandbox-ariana.nyc_taxi_ml.zone_clusters_k4`)
 GROUP BY feature
 ORDER BY feature
 
---checking cluster info, k=4
+--checking cluster info, k=6
 SELECT
   feature,
   ROUND(MAX(IF(centroid_id = 1, numerical_value, NULL)), 1) AS cluster_1,
@@ -119,3 +119,23 @@ SELECT
 FROM ML.CENTROIDS(MODEL `bq-sandbox-ariana.nyc_taxi_ml.zone_clusters_k6`)
 GROUP BY feature
 ORDER BY feature;
+
+--running ml.predict on k=4
+SELECT *
+FROM ML.PREDICT(
+  MODEL `bq-sandbox-ariana.nyc_taxi_ml.zone_clusters_k4`,
+  (
+    SELECT
+      trip_count,
+      avg_total_amount,
+      avg_fare_amount,
+      avg_tip_amount,
+      avg_duration_minutes,
+      avg_distance,
+      pct_rush_hour,
+      pct_night,
+      pct_weekend,
+      pct_adverse_weather
+    FROM `bq-sandbox-ariana.nyc_taxi_ml.mart_zone_clustering`
+  )
+);
